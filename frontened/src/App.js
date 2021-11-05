884937e5ec61c09cedc40eb45d245f32be263a26
kaydons@christensenarms.com <kaydons@christensenarms.com>
2021-11-05 15:13:50 -0600

    frontend update

--- frontened/src/App.js
+++ frontened/src/App.js
@@ -1,14 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import react, { useEffect } from "react";
+import axios from 'axios'
+const baseUrl = "http://localhost:3001"
+
 
 function App() {
+  const [link, setlink] = react.useState("");
+  useEffect(async () => {
+    await setlink(await GetLink())
+  }, []);
+
+
+  async function GetLink() { // const url = process.env.REACT_APP_VEGAAPI_BASEURL + '/getareas'
+    console.log("get link")
+    const url = baseUrl + '/getlink'
+    try {
+      let returnVal = await axios.get(url)
+      console.log(returnVal)
+      console.log(returnVal.data)
+      return returnVal.data.src;
+    }
+    catch {
+      return "https://player.vimeo.com/video/148751763"
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <iframe>Hahaha</iframe>
+        <iframe src={link} width="320" height="240" allowFullScreen allow='autoplay'></iframe>
       </header>
     </div>
   );
 }
 
 export default App;
+
